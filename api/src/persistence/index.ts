7e3f802aeccc5988daa6b11ad0d7575a67d25fdb
German Dario <germangp088@gmail.com>
2020-06-05 21:38:46 -0300

    Implement reconection

--- api/src/persistence/index.ts
+++ api/src/persistence/index.ts
@@ -4,16 +4,34 @@ import config from '../config';
 const connect = async () => {
     const dbHost: any = config.mongo.host;
     const uris: string = `${dbHost.URI}${dbHost.host}/${dbHost.database}`;
+    const db = mongoose.connection;
 
-    mongoose.connect(uris, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-        if (err){
-            console.error(err);
-            process.exit(-1)
-        };
-      
-        console.log('Successfully connected');
-      
+    const performConexion = () => mongoose.connect(uris, { server: { auto_reconnect: true }, useNewUrlParser: true, useUnifiedTopology: true});
+    
+    db.on('error', function(error) {
+        console.error('Error in MongoDb connection: ' + error);
+        mongoose.disconnect();
+        process.exit(-1)
     });
+
+    db.on('connected', function() {
+        console.log('Successfully connected');
+    });
+
+    db.once('open', function() {
+        console.log('MongoDB connection opened!');
+    });
+
+    db.on('reconnected', function () {
+        console.log('MongoDB reconnected!');
+    });
+
+    db.on('disconnected', function() {
+        console.log('MongoDB disconnected!');
+        performConexion();
+    });
+
+    performConexion();
 
     mongoose.Promise = global.Promise;
 }
