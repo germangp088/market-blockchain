7b6efe6a2be7386a063ce28cdb38ddac9b914f0b
German Dario <germangp088@gmail.com>
2020-06-22 00:47:01 -0300

    Remove method

--- api/src/controllers/transaction.controller.ts
+++ api/src/controllers/transaction.controller.ts
@@ -12,48 +12,30 @@ const routerOpts: Router.IRouterOptions = {
 const router: Router = new Router(routerOpts);
 
 router.post("/", async (ctx: Koa.Context, next) => {
-    const userId: string = ctx.request.body.userId;
-    const articuleId: string = ctx.request.body.articuleId;
+  const userId: string = ctx.request.body.userId;
+  const articuleId: string = ctx.request.body.articuleId;
 
-    try {
-      const user: any = await userSchema.findById(userId);
-      const articule: any = await (await articuleSchema.findById(articuleId)).populated("users");
+  try {
+    const user: any = await userSchema.findById(userId);
+    const articule: any = await (await articuleSchema.findById(articuleId)).populated("users");
 
-      console.log({articule});
+    console.log({articule});
 
-      const fromWallet = new EtherAccesor(user.privateKey);
-      const toWallet = new EtherAccesor(articule.user.privateKey);
+    const fromWallet = new EtherAccesor(user.privateKey);
+    const toWallet = new EtherAccesor(articule.user.privateKey);
 
-      const from: string = await fromWallet.getAddress();
-      const to: string = await toWallet.getAddress();
+    const from: string = await fromWallet.getAddress();
+    const to: string = await toWallet.getAddress();
 
-      await fromWallet.transaction(from, to, articule.price);
+    await fromWallet.transaction(from, to, articule.price);
       
-      await articuleSchema.findByIdAndDelete(articuleId);
-    } catch (error) {
-      console.error(error);
-      ctx.throw(HttpStatus.BAD_GATEWAY);
-    }
+    await articuleSchema.findByIdAndDelete(articuleId);
+  } catch (error) {
+    console.error(error);
+    ctx.throw(HttpStatus.BAD_GATEWAY);
+  }
 
-    await next();
-  });
-  
-  router.get("/:id", async (ctx: Koa.Context, next) => {
-    let user: any;
-    
-    try {
-      user = await userSchema.findById(ctx.params.id);
-    } catch (error) {
-      console.error(error);
-      ctx.throw(HttpStatus.BAD_GATEWAY);
-    }
-
-    if(!user){
-      ctx.throw(HttpStatus.NOT_FOUND);
-    }
-
-    ctx.body = user;
-    await next();
+  await next();
 });
 
 export default router;
